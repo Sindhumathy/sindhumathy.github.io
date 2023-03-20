d852df4488d3c650c3a5def2b91c8b2821c1e21d
MohamedSuja <mohammed@codelantic.com>
2023-03-20 20:39:17 +0530

    change page ban

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,6 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const greetingValue = urlParams.get("id");
 const bannerContainer = document.getElementById("banner");
+const goalsContainer = document.getElementById("goals-container");
 console.log(greetingValue, 777777777777777);
 
 /// banner images
@@ -16,6 +17,22 @@ const returnBanner = (data_val) => {
   );
 };
 
+///
+const goalsCard = (data_val) => {
+  return data_val.map(
+    (val) =>
+      `<div
+      class="col-lg-4 col-md-6 d-md-flex align-items-md-stretch"
+      data-aos="fade-up"
+    >
+      <div class="count-box">
+        <i class="${val.iconName}"></i>                
+        <p>${val.goal}</p>
+      </div>
+    </div>`
+  );
+};
+
 fetch("../../data/portfolio-data.json")
   .then((response) => response.json())
   .then((data) => {
@@ -27,10 +44,19 @@ fetch("../../data/portfolio-data.json")
     const bannerImages = data.find(
       (item) => item.id === parseInt(greetingValue)
     ).banner;
+    const description = data.find(
+      (item) => item.id === parseInt(greetingValue)
+    ).description;
+
+    const goals = data.find(
+      (item) => item.id === parseInt(greetingValue)
+    ).goals;
 
     console.log(bannerImages);
 
     bannerContainer.innerHTML = returnBanner(bannerImages).join("");
     document.getElementById("title").innerHTML = title;
     document.getElementById("logo-mage").src = logo;
+    document.getElementById("description-text").innerHTML = description;
+    goalsContainer.innerHTML = goalsCard(goals).join("");
   });
